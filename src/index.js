fb21b57c3e9a714208596d7d3cacc61097d8da59
R.J <rjschodowski@hotmail.com>
2021-06-28 15:22:52 -0400

    button and  label added to component

--- src/index.js
+++ src/index.js
@@ -2,7 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-    return <div>Forget-Me-Shop</div>;
+
+    const buttonText = 'Click me dude!';
+
+    return (
+        <div>
+            <label class="label" for="name">
+                Enter name:
+            </label>
+            <input id="name" type="text" />
+            <button style={{ backgroundColor: 'blue', color: 'white' }}>{buttonText}</button>
+        </div>
+    )
 };
 
 ReactDOM.render(
